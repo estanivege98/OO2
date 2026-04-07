637529246d1a8f588dd9b04b62bf74330a0c754b
estanivege98 <estanislao.vegetti239518@alumnos.info.unlp.edu.ar>
2026-04-06 21:37:30 -0300

    Ejercicio 1 y 2 hechos

---
+++ Practicas/CuadernilloRefactoring/Ejercicio2/CharRing.java
@@ -0,0 +1,53 @@
+/*
+* Tareas:
+Se quiere aplicar el refactoring Rename Variable sobre la variable result que se usa en la
+* línea 18 con el nuevo nombre currentPosition.
+¿Cómo queda el código final y qué inconveniente se podría encontrar?
+*/
+public class CharRing extends Object {
+    char[] source;
+    int nextIndex;
+
+    public CharRing(String srcString) {
+        char currentCharIndex;
+        source = new char[srcString.length()];
+        srcString.getChars(0, srcString.length(), source, 0);
+        currentCharIndex = 0;
+        nextIndex = currentCharIndex;
+    }
+
+    public char next() {
+        int currentCharIndex;
+        if (nextIndex >= source.length)
+            nextIndex = 0;
+        currentCharIndex = nextIndex++;
+        return source[currentCharIndex];
+    }
+  }
+/* Al cabiar el nombre de la variable a uno que sea mas especifico, el idx tambien presenta un nombre
+* confunso, cambie de idx a nextIndex. Esto tambien lo hice en CharRing.
+* Existen otros bad smells como:
+* herencia innecesaria: la herencia de la clase Object es totalmente innesario
+* Posible bug en srcString: si este es vacio, devuelve source[0] que devuelve ArrayIndexOutOfBOunds
+* Encapsulamiento debil: tanto source como nextIndex no son privados
+* currentCharIndex es varible temporal inncesaria
+* */
+
+public class CharRing {
+    private final char[] source;
+    private int nextIndex;
+
+    public CharRing(String srcString){
+        if (srcString != null) {
+            srcString.getChars(0, srcString.length(), source, 0)
+        }
+        source = new char[srcString.length()];
+        nextIndex = 0;
+    }
+
+    public char next(){
+        if (nextIndex >= source.length)
+            nextIndex = 0;
+        return source[nextIndex++];
+    }
+}
